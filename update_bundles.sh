076bab451454a145aa3c5a2e70feede371639c07
Troels Ugilt Jensen <tujensen@gmail.com>
2018-06-20 14:53:33 +0200

    OS2DISP-134: Fixed admin

--- update_bundles.sh
+++ update_bundles.sh
@@ -1,1 +1,2 @@
-composer update os2display/core-bundle os2display/admin-bundle os2display/default-template-bundle os2display/media-bundle itk-os2display/aarhus-data-bundle itk-os2display/aarhus-second-template-bundle aakb/os2display-aarhus-templates itk-os2display/template-extension-bundle itk-os2display/lokalcenter-template-bundle
+composer="$(which composer)"
+php -d memory_limit=-1 $composer update os2display/core-bundle os2display/admin-bundle os2display/default-template-bundle os2display/media-bundle itk-os2display/aarhus-data-bundle itk-os2display/aarhus-second-template-bundle aakb/os2display-aarhus-templates itk-os2display/template-extension-bundle itk-os2display/lokalcenter-template-bundle itk-os2display/vimeo-bundle itk-os2display/campaign-bundle
