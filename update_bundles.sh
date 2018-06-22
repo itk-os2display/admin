2f38cf373411904cb1fc0ffc5c05685d5a798634
Troels Ugilt Jensen <tujensen@gmail.com>
2018-06-22 09:48:43 +0200

    OS2DISP-135: Updated bundles

--- update_bundles.sh
+++ update_bundles.sh
@@ -3,4 +3,4 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 composer="$(which composer)"
-php -d memory_limit=-1 $composer update os2display/core-bundle os2display/admin-bundle os2display/default-template-bundle os2display/media-bundle itk-os2display/aarhus-data-bundle itk-os2display/aarhus-second-template-bundle aakb/os2display-aarhus-templates itk-os2display/template-extension-bundle itk-os2display/lokalcenter-template-bundle itk-os2display/vimeo-bundle itk-os2display/campaign-bundle
+php -d memory_limit=-1 $composer update symfony/symfony os2display/core-bundle os2display/admin-bundle os2display/default-template-bundle os2display/media-bundle itk-os2display/aarhus-data-bundle itk-os2display/aarhus-second-template-bundle aakb/os2display-aarhus-templates itk-os2display/template-extension-bundle itk-os2display/lokalcenter-template-bundle itk-os2display/vimeo-bundle itk-os2display/campaign-bundle
